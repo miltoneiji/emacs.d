97aabc1f2346756695045a16d51063f5db126042
Milton Eiji Takamura <miltontakamura@gmail.com>
2021-12-17 10:10:34 -0300

    Ruby

--- init.el
+++ init.el
@@ -288,12 +288,25 @@
 ;;; lang ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package lsp-mode
+  :straight t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((ruby-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :straight t
+  :commands lsp-ui-mode)
+
+(use-package lsp-ivy
+  :straight t
+  :commands lsp-ivy-workspace-symbol)
+
 ;;;;;;;;;;
 ;; Ruby ;;
 ;;;;;;;;;;
-
-(use-package inf-ruby
-  :straight t)
 
 ;;;;;;;;;;;;;
 ;; Clojure ;;
