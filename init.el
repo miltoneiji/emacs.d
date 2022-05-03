102a55f2f224ddfbb8c22b433e12608840e79786
Milton Eiji Takamura <miltontakamura@gmail.com>
2022-05-03 21:28:34 +0100

    sqlite orgbabel

--- init.el
+++ init.el
@@ -265,15 +265,28 @@
 (use-package org
   :hook (org-mode . tk/org-mode-setup)
   :config
+  (map-local! org-mode-map
+    "e"   '(:ignore t :which-key "eval")
+    "e f" 'org-babel-execute-maybe
+    "e b" 'org-babel-execute-buffer
+    "'"   'org-edit-special
+    "c"   'org-babel-remove-result-one-or-many)
+
   (defun tk/org-mode-setup ()
     ;; disable automatic line breaking
     (auto-fill-mode 0)
+    ;;
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((emacs-lisp . t)
+       (sqlite . t)))
     ;; wrap lines
     (visual-line-mode t)
     ;; indent text according to outline structure
     (org-indent-mode))
   :custom
   (org-ellipsis "...")
+  (org-confirm-babel-evaluate nil)
   (org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAIT" "CODE-REVIEW" "HOLD" "|" "DONE" "DELEGATED" "CANCELLED")))
   ;; =M-x list-colors-display= to see colors
   (org-todo-keyword-faces '(("TODO" . "chartreuse4")
