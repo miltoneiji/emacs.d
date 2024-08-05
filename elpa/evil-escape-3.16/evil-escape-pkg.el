fc0e1d668046f16f8217f211acdff976a1f18c0a
Milton Eiji Takamura <miltontakamura@gmail.com>
2024-08-05 20:32:58 -0300

    a

---
+++ elpa/evil-escape-3.16/evil-escape-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from evil-escape.el  -*- no-byte-compile: t -*-
+(define-package "evil-escape" "3.16" "Escape from anything with a customizable key sequence" '((emacs "24") (evil "1.0.9") (cl-lib "0.5")) :commit "3c335a5709b1abb08687960038746b36add465f9" :authors '(("Sylvain Benner" . "sylvain.benner@gmail.com")) :maintainer '("Sylvain Benner" . "sylvain.benner@gmail.com") :keywords '("convenience" "editing" "evil") :url "https://github.com/emacsorphanage/evil-escape")
---
+++ elpa/smartparens-1.11.0/smartparens-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from smartparens.el  -*- no-byte-compile: t -*-
+(define-package "smartparens" "1.11.0" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs." 'nil :commit "4873352b5d0a1c5142658122de1b6950b8fe7e4d" :authors '(("Matus Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matus Goljer" . "matus.goljer@gmail.com") :keywords '("abbrev" "convenience" "editing") :url "https://github.com/Fuco1/smartparens")
